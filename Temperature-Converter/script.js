4c3d62d14493107bcc61ea1d2fde624f1fe7fbd5
krivorotovss <s.krivorotov@yandex.ru>
2023-01-25 23:50:53 +0700

    Fixed Temperature Converter

--- Temperature-Converter/script.js
+++ Temperature-Converter/script.js
@@ -5,11 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var convertButton = document.getElementById("convert-button");
 
-    convertButton.addEventListener("click", function (e) {
-        var celsiusTextItem = celsiusTextBox.value.trim();
+    convertButton.addEventListener("click", function () {
+        var celsiusText = celsiusTextBox.value.trim();
 
-        fahrenheitTextBox.value = (9 * Number(celsiusTextItem) / 5 + 32).toFixed(2);
-        kelvinTextBox.value = (273.15 + Number(celsiusTextItem)).toFixed(2);
+        var celsiusTemperature = Number(celsiusText);
+
+        if (celsiusText.length == 0 || isNaN(celsiusTemperature)) {
+            celsiusTextBox.classList.add("error-message");
+            celsiusTextBox.value = "Введите число";
+            return;
+        }
+
+        fahrenheitTextBox.value = (9 * celsiusTemperature / 5 + 32).toFixed(2);
+        kelvinTextBox.value = (273.15 + celsiusTemperature).toFixed(2);
     });
-
 });
