7304bce039ee224697d2f790b0af1fb436fd0b38
krivorotovss <s.krivorotov@yandex.ru>
2023-01-26 21:07:00 +0700

    Create TODO List

---
+++ Todo-List/script.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var addButton = document.getElementById("add-button");
+    var newTodoTextInput = document.getElementById("new-todo-text");
+    var todoList = document.getElementById("todo-list");
+    var form = document.getElementById("form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+
+    addButton.addEventListener("click", function () {
+        var newTodoText = newTodoTextInput.value.trim();
+        newTodoTextInput.classList.remove("invalid");
+
+        if (newTodoText.length === 0) {
+            newTodoTextInput.classList.add("invalid");
+            return;
+        }
+
+        function setEditMode() {
+            todoItem.innerHTML = "<input class='edit-todo-item' type='text'>\
+                <button class='save-button' type='button'>Save</button>\
+                <button class='cancel-button' type='button'>Cancel</button>\
+                <div style = 'display: none' class='edit-error-message'>Field is required</div>";
+
+            todoItem.querySelector(".edit-todo-item").value = newTodoText;
+
+            todoItem.querySelector(".cancel-button").addEventListener("click", function () {
+                setViewMode();
+            });
+
+            todoItem.querySelector(".save-button").addEventListener("click", function () {
+                var editedTodoText = todoItem.querySelector(".edit-todo-item").value.trim();
+                var editErrorMessage = todoItem.querySelector('.edit-error-message');
+
+                if (editedTodoText.length === 0) {
+                    todoItem.querySelector(".edit-todo-item").classList.add("invalid");
+                    editErrorMessage.style.display = 'block';
+                    editErrorMessage.style.color = "#f00";
+                    return;
+                }
+
+                newTodoText = editedTodoText;
+                setViewMode();
+            });
+        }
+
+        function setViewMode() {
+            todoItem.innerHTML = "<span class='todo-item-text'></span>\
+                <button class='edit-button' type='button'>Edit</button>\
+                <button class='delete-button' type='button'>Delete</button>";
+
+            todoItem.querySelector(".todo-item-text").textContent = newTodoText;
+
+            todoItem.querySelector(".delete-button").addEventListener("click", function () {
+                todoItem.remove();
+            });
+
+            todoItem.querySelector(".edit-button").addEventListener("click", function () {
+                setEditMode();
+            });
+        }
+
+        var todoItem = document.createElement("li");
+        todoItem.classList.add("todo-item");
+
+        setViewMode();
+
+        todoList.appendChild(todoItem);
+        todoList.appendChild(todoItem);
+    })
+});
