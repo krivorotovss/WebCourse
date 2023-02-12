f2facfcbcbaed25f09b303f56b9cc0970ba52916
krivorotovss <s.krivorotov@yandex.ru>
2023-02-12 20:57:01 +0700

    Fixed JQ Todo List

--- JQ-Todo-List/script.js
+++ JQ-Todo-List/script.js
@@ -17,27 +17,31 @@ $(function () {
             return;
         }
 
+        var todoItem = $("<li>").addClass("todo-item");
+
         function setEditMode() {
             todoItem.html("<input class='edit-todo-item' type='text'>\
+                <div class='save-cancel-buttons'>\
                 <button class='save-button' type='button'>Save</button>\
                 <button class='cancel-button' type='button'>Cancel</button>\
-                <div style='display: none' class='edit-error-message'>Field is required</div>");
+                <div style='display: none' class='edit-error-message'>Field is required</div>\
+                </div>");
 
-            todoItem.find(".edit-todo-item").val(newTodoText);
+            var editTodoItem = todoItem.find(".edit-todo-item")
+
+            editTodoItem.val(newTodoText);
 
             todoItem.find(".cancel-button").click(function () {
                 setViewMode();
             });
 
             todoItem.find(".save-button").click(function () {
-                var editedTodoText = todoItem.find(".edit-todo-item").val().trim();
+                var editedTodoText = editTodoItem.val().trim();
                 var editErrorMessage = todoItem.find(".edit-error-message");
 
                 if (editedTodoText.length === 0) {
-                    todoItem.find(".edit-todo-item").addClass("invalid");
-                    todoItem.find(".edit-todo-item").addClass("edit-error-message");
+                    editTodoItem.addClass("invalid");
                     editErrorMessage.show();
-                    todoItem.find(".edit-todo-item").removeClass("edit-error-message");
                     return;
                 }
 
@@ -48,8 +52,10 @@ $(function () {
 
         function setViewMode() {
             todoItem.html("<span class='todo-item-text'></span>\
+                <div class='edit-delete-buttons'>\
                 <button class='edit-button' type='button'>Edit</button>\
-                <button class='delete-button' type='button'>Delete</button>");
+                <button class='delete-button' type='button'>Delete</button>\
+                <div>");
 
             todoItem.find(".todo-item-text").text(newTodoText);
 
@@ -61,8 +67,6 @@ $(function () {
                 setEditMode();
             });
         }
-
-        var todoItem = $("<li>").addClass("todo-item");
 
         setViewMode();
 
