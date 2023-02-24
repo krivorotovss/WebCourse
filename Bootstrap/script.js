1bcb4a6de3b2efcb1870f720e880b64848be3f69
krivorotovss <s.krivorotov@yandex.ru>
2023-02-25 02:48:52 +0700

    Fixed Bootstrap

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -26,15 +26,15 @@ $(function () {
         newTodoTextInput.val("");
 
         function setEditMode() {
-            todoItem.html("<div class='row mb-2 mt-2'>" +
-                "<div class='col-1 col-lg-1'></div>" +
-                "<div class='col-6 col-lg-8'>" +
-                "<input class='edit-todo-item form-control' type='text' >" +
-                "<div class='edit-error-message invalid-feedback'>Field is required</div>" +
+            todoItem.html("<div class='row mb-2 mt-2 justify-content-center'>" +
+                "<div class='col-lg-1'></div>" +
+                "<div class='col-10 col-lg-7'>" +
+                    "<input class='edit-todo-item form-control mb-2' type='text' >" +
+                    "<div class='edit-error-message invalid-feedback mb-2'>Field is required</div>" +
                 "</div>" +
-                "<div class='col-4 col-lg-3 text-start'>" +
-                "<button class='save-button btn btn-success me-2' type='button'>Save</button>" +
-                "<button class='cancel-button btn btn-secondary btn-outline-light' type='button'>Cancel</button></div>" +
+                "<div class='col-10 col-lg-3 mb-3 btn-group d-lg-block' role='group' aria-label='edit buttons'>" +
+                    "<button class='save-button btn btn-success me-lg-2 me-2' type='button'>Save</button>" +
+                    "<button class='cancel-button btn btn-secondary btn-outline-light' type='button'>Cancel</button></div>" +
                 "</div>");
 
             todoItem.find(".edit-todo-item").val(newTodoText);
@@ -45,7 +45,6 @@ $(function () {
 
             todoItem.find(".save-button").click(function () {
                 var editedTodoText = todoItem.find(".edit-todo-item").val().trim();
-                var editErrorMessage = todoItem.find(".edit-error-message");
 
                 if (editedTodoText.length === 0) {
                     todoItem.find(".edit-todo-item").addClass("is-invalid");
@@ -58,12 +57,13 @@ $(function () {
         }
 
         function setViewMode() {
-            todoItem.html("<div class='row mb-2 mt-2'>" +
-                "<div class='col-1 col-lg-1'></div>" +
-                "<div class='col-6 col-lg-8 text-start'><span class='todo-item-text'></span></div>" +
-                "<div class='col-5 col-lg-3 text-start'>" +
-                "<button class='edit-button btn btn-secondary btn-outline-light me-2' type='button'>Edit</button>" +
-                "<button class='delete-button btn btn-danger' type='button'>Delete</button></div>" +
+            todoItem.html("<div class='row mb-2 mt-2 justify-content-center'>" +
+                    "<div class='col-0 col-lg-1'></div>" +
+                    "<div class='col-10 col-lg-7 text-start mb-2 ms-4 ms-lg-0'><span class='todo-item-text'></span></div>" +
+                    "<div class='col-10 col-lg-3 mb-3 btn-group d-lg-block' role='group' aria-label='edit buttons'>" +
+                        "<button class='edit-button btn btn-secondary btn-outline-light me-2' type='button'>Edit</button>" +
+                        "<button class='delete-button btn btn-danger' type='button'>Delete</button>" +
+                    "</div>" +
                 "</div>");
 
             todoItem.find(".todo-item-text").text(newTodoText);
